b2db064ded3135f8340675d75c9239283fbcc8f6
Ben Tan <btan87@gmail.com>
2021-05-19 20:30:58 -0700

    added ALL button category

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -41,6 +41,10 @@ function CategoryMenu() {
   return (
     <div>
       <h2>Choose a Category:</h2>
+        <button onClick={() => {
+            handleClick()}}>
+        ALL
+        </button>
       {categories.map(item => (
         <button
           key={item._id}
