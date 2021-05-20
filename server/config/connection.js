8d164377307223981996ea0840832577017e0cbe
Ben Tan <btan87@gmail.com>
2021-05-19 22:27:16 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/groceryapp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/thefoodchain', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
