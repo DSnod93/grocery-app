473d2b40418964542180ec2da2ace5062ceaf44f
Ben Tan <btan87@gmail.com>
2021-05-19 18:40:21 -0700

    fixed the signup component

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+
 import { useMutation } from '@apollo/react-hooks';
 import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
@@ -29,59 +29,60 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">
-        ← Go to Login
-      </Link>
+    <div className="container">
+        <div className="col">
 
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
+          <h4>Signup</h4>
+          <br/>
+          <form className="col s12" onSubmit={handleFormSubmit}>
+            <div className="input-field col s6">
+
+              <input
+                name="firstName"
+                type="text"
+                id="firstName"
+                onChange={handleChange}
+              />
+              <label htmlFor="firstName">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input
+                name="lastName"
+                type="text"
+                id="lastName"
+                onChange={handleChange}
+              />
+              <label htmlFor="lastName">Last Name</label>
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex-row space-between my-2">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex-row flex-end">
+              <button className="btn waves-effect waves-light" type="submit">
+                Submit
+                <i className="material-icons right">send</i>
+              </button>
+            </div>
+          </form>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
+
     </div>
   );
 
